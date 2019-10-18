d7e91fa876c0e76fcdbe4f6937e3b54b8a0e5859
jagvet <54946914+jagvet@users.noreply.github.com>
2019-10-18 20:38:04 +0200

    final

--- Program.cs
+++ Program.cs
@@ -10,50 +10,78 @@ namespace fightsimv1
     {
         static void Main(string[] args)
         {
-            Fighter f1 = new Fighter();
-            Fighter f2 = new Fighter();
-            
+            bool keepPlaying = true;
 
-            Console.WriteLine("Hello and welcome to a fight simulator");
-            Console.WriteLine("Here you will be able to make people figt");
-            Console.WriteLine();
-
-            Console.WriteLine("Please name your first fighter");
-
-            f1.name = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("The first fighters name is " + f1.name);
-
-            Console.WriteLine();
-            Console.WriteLine("Please name your second fighter");
-
-            f2.name = Console.ReadLine();
-
-            Console.WriteLine();
-            Console.WriteLine("The second fighters name is " + f2.name);
-            
-            Console.WriteLine(f1.name + " has " + f1.hp + " hp");
-            Console.WriteLine(f2.name + " has " + f2.hp + " hp");
-
-            while(f1.isAlive() == true && f2.isAlive() == true)
+            while (keepPlaying == true)
             {
-                Console.WriteLine("Fight");
+                Fighter f1 = new Fighter();
+                Fighter f2 = new Fighter();
 
-                f2.Hurt(f1.Attack());
-                f1.Hurt(f2.Attack());
 
-                Console.WriteLine(f1.name + " has " + f1.hp + " hp left");
-                Console.WriteLine(f2.name + " has " + f2.hp + " hp left");
+                Console.WriteLine("Hello and welcome to a fight simulator");
+                Console.WriteLine("Here you will be able to make people fight");
+                Console.WriteLine();
 
-                f1.isAlive();
-                f2.isAlive();
+                Console.WriteLine("Please name your first fighter");
+                f1.name = Console.ReadLine();
 
-                Console.ReadLine();
+                Console.Clear();
+
+                Console.WriteLine("Please name your second fighter");
+                f2.name = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("The two fighters are " + f1.name + " and " + f2.name);
+                Console.WriteLine();
+
+                Console.WriteLine(f1.name + " has " + f1.hp + " hp");
+                Console.WriteLine(f2.name + " has " + f2.hp + " hp");
+                Console.WriteLine();
+
+                while (f1.isAlive() == true && f2.isAlive() == true)
+                {
+                    Console.WriteLine("'Fight'");
+                    Console.WriteLine();
+
+                    f2.Hurt(f1.Attack());
+                    f1.Hurt(f2.Attack());
+
+                    Console.WriteLine(f1.name + " has " + f1.GetHp() + " hp left");
+                    Console.WriteLine(f2.name + " has " + f2.GetHp() + " hp left");
+
+                    f1.isAlive();
+                    f2.isAlive();
+
+                    Console.ReadLine();
+                }
+
+                if (f1.isAlive() == true && f2.isAlive() == false)
+                {
+                    Console.WriteLine("Congratulations " + f1.name + "! You win!");
+                }
+                if (f1.isAlive() == false && f2.isAlive() == true)
+                {
+                    Console.WriteLine("Congratulations " + f2.name + "! You win!");
+                }
+                if (f1.isAlive() == false && f2.isAlive() == false)
+                {
+                    Console.WriteLine("They knocked each other out! Nobody win!");
+                }
+
+                Console.WriteLine("Do you want to play again?");
+                string answer = Console.ReadLine();
+                answer = answer.ToLower();
+                while(answer != "yes" && answer != "no")
+                {
+                    Console.WriteLine("Yes or no?");
+                    answer = Console.ReadLine();
+                }
+                if(answer == "no")
+                {
+                    keepPlaying = false;
+                }
+                Console.Clear();
             }
-            Console.WriteLine("yay");
-
-            Console.ReadLine();
         }
     }
 }
